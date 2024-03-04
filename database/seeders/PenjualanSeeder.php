9906a1381a51601db5fc5d6d2b986af8757026cf
Achmad Mufid <achmadmufid.tbn@gmail.com>
2024-03-04 19:03:00 +0700

    seeder untuk tabel t_penjualan

---
+++ database/seeders/PenjualanSeeder.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PenjualanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'penjualan_id' => 1,
+                'user_id' => 3,
+                'pembeli' => 'Bagas',
+                'penjualan_kode' => 'JL001',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 2,
+                'user_id' => 3,
+                'pembeli' => 'Ani',
+                'penjualan_kode' => 'JL002',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 3,
+                'user_id' => 3,
+                'pembeli' => 'Budi',
+                'penjualan_kode' => 'JL003',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 4,
+                'user_id' => 3,
+                'pembeli' => 'Nanda',
+                'penjualan_kode' => 'JL004',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 5,
+                'user_id' => 3,
+                'pembeli' => 'Dony',
+                'penjualan_kode' => 'JL005',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 6,
+                'user_id' => 3,
+                'pembeli' => 'Wulan',
+                'penjualan_kode' => 'JL006',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 7,
+                'user_id' => 3,
+                'pembeli' => 'Chandra',
+                'penjualan_kode' => 'JL007',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 8,
+                'user_id' => 3,
+                'pembeli' => 'Arya',
+                'penjualan_kode' => 'JL008',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 9,
+                'user_id' => 3,
+                'pembeli' => 'Dea',
+                'penjualan_kode' => 'JL009',
+                'penjualan_tanggal' => now(),
+            ],
+            [
+                'penjualan_id' => 10,
+                'user_id' => 3,
+                'pembeli' => 'Galih',
+                'penjualan_kode' => 'JL010',
+                'penjualan_tanggal' => now(),
+            ]
+        ];
+        DB::table('t_penjualan')->insert($data);
+    }
+}
