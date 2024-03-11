0dfe73f410a87c5eb974fbd18b9f09d744487cae
Achmad Mufid <achmadmufid.tbn@gmail.com>
2024-03-11 12:50:57 +0700

    praktikum 2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,16 +12,10 @@
     <h1>Data User</h1>
     <table border="1" cellpading="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>jumlah pengguna</th>
         </tr>
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $data}}</td>
         </tr>
 
     </table>
