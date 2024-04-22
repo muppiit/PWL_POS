7fdba76f8961bb1cdd84da708345973eb82120a2
Achmad Mufid <achmadmufid.tbn@gmail.com>
2024-04-22 22:35:37 +0700

    joobsheet 8

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,14 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticaple;
 
-class UserModel extends Model
+class UserModel extends Authenticaple
 {
     use HasFactory;
     protected $table = "m_user";
     protected $primaryKey = "user_id";
 
     protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
+
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
